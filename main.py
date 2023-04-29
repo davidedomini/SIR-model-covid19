c16a64da7784dd89b5b63d6da042727b55b3bc43
dom <davide.domini2@unibo.it>
2023-04-29 09:18:10 +0200

    feat: add data downloading and plotting

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
+    df = pd.read_csv(url, usecols=["data", "nuovi_positivi"])
+    dfinit = df.iloc[:140]
+    print(dfinit)
+    plt.figure(figsize=(8, 4))
+    plt.plot(dfinit.nuovi_positivi, label="nuovi positivi")
+    plt.legend()
+    plt.show()
+
